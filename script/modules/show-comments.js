4e6678b48928897c692482bf918d34132848cbcb
rafaelhbastos <rafaelhenribastos12@gmail.com>
2021-06-17 13:50:13 -0300

    js function show-comment done

---
+++ script/modules/show-comments.js
@@ -0,0 +1,36 @@
+export default function showComments() {
+  const topic = document.querySelector('.topic-commented');
+
+  function comeComments() {
+    const footer = document.querySelector('.topic-commented .topic-footer');
+    const footerText = document.querySelectorAll('.topic-commented .topic-footer p');
+    const comments = document.querySelectorAll('[data-show = "appear"]');
+
+    if(!comments[0].classList.contains('active')) {
+      footer.style.marginTop = '28px';
+      footerText.forEach(p => {
+        p.innerText =  p.innerText.replace('1', '4') + 's';
+      });
+
+      comments.forEach(comment => {
+        comment.classList.add('active');
+      });
+
+    } else {
+      footer.style.marginTop = '11px';
+      footerText.forEach(p => {
+        let text = new String(p.innerText);
+        text = text.replace('4', '1');
+        text = text.slice(0,-1);
+        p.innerText = text;
+      });
+
+      comments.forEach(comment => {
+        comment.classList.remove('active');
+      });
+    }
+  }
+
+  topic.addEventListener('click', comeComments);
+  
+}
--- script/script.js
+++ script/script.js
@@ -1,9 +1,11 @@
 import animeInit from "./modules/anime-init.js";
 import seeAllText from "./modules/see-all-text.js";
-import sendTopic from "./modules/send-topic.js";
 import sideBar from "./modules/side-bar.js";
+import sendTopic from "./modules/send-topic.js";
+import showComments from "./modules/show-comments.js";
 
 animeInit();
 seeAllText();
 sideBar();
 sendTopic();
+showComments();
