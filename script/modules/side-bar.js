8293a70072517f8c4197b945862ec6c13499f79e
rafaelhbastos <rafaelhenribastos12@gmail.com>
2021-06-18 14:21:30 -0300

    media of side-bar done

--- script/modules/side-bar.js
+++ script/modules/side-bar.js
@@ -1,14 +1,24 @@
 export default function sideBar() {
   const navigation = document.querySelectorAll('.menu-nav .topics li a');
-  
+  const user = document.querySelector('.menu-nav .topics li p');
+
   function addActive(element) {
+    user.classList.remove('active');
     navigation.forEach(item => {
       item.classList.remove('active');
     });
     element.classList.add('active');
   }
 
+  function addActiveUser(element) {
+    navigation.forEach(item => {
+      item.classList.remove('active');
+    });
+    user.classList.add('active');
+  }
+
   navigation.forEach(element => {
     element.addEventListener('click', () => addActive(element));
+    user.addEventListener('click', () => addActiveUser(user));
   });
 }
--- script/script.js
+++ script/script.js
@@ -3,9 +3,11 @@ import seeAllText from "./modules/see-all-text.js";
 import sideBar from "./modules/side-bar.js";
 import sendTopic from "./modules/send-topic.js";
 import showComments from "./modules/show-comments.js";
+import activeMenu from "./modules/active-menu.js";
 
 animeInit();
 seeAllText();
 sideBar();
 sendTopic();
 showComments();
+activeMenu();
